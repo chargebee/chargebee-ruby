c8ab7f9fb95455fc72f5d343aad9aa77b8cab5d3
kps <kps@chargebee.com>
2012-08-11 18:17:51 +0530

    Added redirecturl and embed

--- lib/chargebee/models/plan.rb
+++ lib/chargebee/models/plan.rb
@@ -2,7 +2,7 @@ module ChargeBee
   class Plan < Model
 
   attr_accessor :id, :name, :invoice_name, :price, :period, :period_unit, :trial_period, :trial_period_unit,
-  :setup_cost, :status, :archived_at
+  :setup_cost, :status, :archived_at, :redirect_url
 
   # OPERATIONS
   #-----------
