2cddf1ea4e01cce429ed6e67363102c53b96a8aa
Thiyagu <thiyagu@bubblepath.com>
2012-09-29 18:11:00 +0530

    Transaction.invoice_id removed

--- lib/chargebee/models/transaction.rb
+++ lib/chargebee/models/transaction.rb
@@ -1,9 +1,8 @@
 module ChargeBee
   class Transaction < Model
 
-  attr_accessor :id, :subscription_id, :gateway, :description, :invoice_id, :type, :date, :amount,
-  :id_at_gateway, :masked_card_number, :error_code, :error_text, :refunded_tx_id, :voided_at,
-  :status
+  attr_accessor :id, :subscription_id, :gateway, :description, :type, :date, :amount, :id_at_gateway,
+  :masked_card_number, :error_code, :error_text, :refunded_tx_id, :voided_at, :status
 
   # OPERATIONS
   #-----------
