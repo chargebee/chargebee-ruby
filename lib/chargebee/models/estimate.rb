d6e437bc800b98a907a4f9c2ed20d45a1ea6ffde
Thiyagu <thiyagu@bubblepath.com>
2013-02-13 14:31:47 +0530

    Estimates support

---
+++ lib/chargebee/models/estimate.rb
@@ -0,0 +1,31 @@
+module ChargeBee
+  class Estimate < Model
+
+    class LineItem < Model
+      attr_accessor :date_from, :date_to, :unit_amount, :quantity, :tax, :amount, :description
+    end
+
+    class Discount < Model
+      attr_accessor :amount, :description
+    end
+
+    class Tax < Model
+      attr_accessor :amount, :description
+    end
+
+  attr_accessor :created_at, :recurring, :subscription_id, :subscription_status, :term_ends_at,
+  :collect_now, :amount, :sub_total, :line_items, :discounts, :taxes
+
+  # OPERATIONS
+  #-----------
+    
+  def self.create_subscription(params, env=nil)
+    Request.send('post', "/estimates/create_subscription", params, env)
+  end    
+
+  def self.update_subscription(params, env=nil)
+    Request.send('post', "/estimates/update_subscription", params, env)
+  end    
+
+  end # ~Estimate
+end # ~ChargeBee    
