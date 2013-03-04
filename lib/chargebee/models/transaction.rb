b6e55f3e9e8e593de6b6742803e8efb492d73acf
Thiyagu <thiyagu@bubblepath.com>
2013-03-04 13:09:26 +0530

    Transaction model changes

--- lib/chargebee/models/transaction.rb
+++ lib/chargebee/models/transaction.rb
@@ -2,7 +2,8 @@ module ChargeBee
   class Transaction < Model
 
   attr_accessor :id, :subscription_id, :payment_method, :gateway, :description, :type, :date,
-  :amount, :id_at_gateway, :error_code, :error_text, :refunded_tx_id, :voided_at, :status, :masked_card_number
+  :amount, :id_at_gateway, :error_code, :error_text, :voided_at, :status, :masked_card_number,
+  :refunded_txn_id
 
   # OPERATIONS
   #-----------
