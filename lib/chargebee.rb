9ac0f2f77f3a9160b8b98f19ec07d00be8315054
rraman <rr@chargebee.com>
2013-09-28 20:05:58 +0530

    Version bump to 1.2.6

--- lib/chargebee.rb
+++ lib/chargebee.rb
@@ -24,7 +24,7 @@ require File.dirname(__FILE__) + '/chargebee/models/coupon_code'
 
 module ChargeBee
 
-  VERSION = '1.2.5'
+  VERSION = '1.2.6'
  
   @@default_env = nil
   @@verify_ca_certs = true
