77afb82eb96bac8f54c9b23518e07d76c54cf7f2
rraman <rr@chargebee.com>
2014-01-16 17:49:51 +0530

    Version bump to 1.3.1 and Changelog update.

--- lib/chargebee.rb
+++ lib/chargebee.rb
@@ -21,10 +21,11 @@ require File.dirname(__FILE__) + '/chargebee/models/plan'
 require File.dirname(__FILE__) + '/chargebee/models/addon'
 require File.dirname(__FILE__) + '/chargebee/models/coupon'
 require File.dirname(__FILE__) + '/chargebee/models/coupon_code'
+require File.dirname(__FILE__) + '/chargebee/models/comment'
 
 module ChargeBee
 
-  VERSION = '1.3.0'
+  VERSION = '1.3.1'
  
   @@default_env = nil
   @@verify_ca_certs = true
