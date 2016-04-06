076c7487f409b886ed51a77f0132dd557633cbcc
vaibhav <vaibhav@chargebee.com>
2016-04-06 08:37:41 +0000

    *api_version* attribute is added to the Event resource.
    Chargebee supports multiple [API versions](https://apidocs.chargebee.com/docs/api#versions) now. The *api_version* attribute indicates the API version based on which the event content is structured. More details here:
    https://apidocs.chargebee.com/docs/api/v1/events

--- lib/chargebee/environment.rb
+++ lib/chargebee/environment.rb
@@ -1,17 +1,17 @@
 module ChargeBee
   class Environment
-    
+    API_VERSION = "v1" 
     attr_accessor :api_key, :site
-    attr_reader :api_endpoint
+    attr_reader :api_endpoint 
       
     def initialize(options)
       [:api_key, :site].each do |attr|
         instance_variable_set "@#{attr}", options[attr]
       end
       if($CHARGEBEE_DOMAIN == nil)
-        @api_endpoint = "https://#{@site}.chargebee.com/api/v1"
+        @api_endpoint = "https://#{@site}.chargebee.com/api/#{API_VERSION}"
       else
-        @api_endpoint = "#{$ENV_PROTOCOL == nil ? "http": "https"}://#{@site}.#{$CHARGEBEE_DOMAIN}/api/v1"
+        @api_endpoint = "#{$ENV_PROTOCOL == nil ? "http": "https"}://#{@site}.#{$CHARGEBEE_DOMAIN}/api/#{API_VERSION}"
       end
     end
 
