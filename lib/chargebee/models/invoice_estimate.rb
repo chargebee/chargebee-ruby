cf09c99d88edc6dbbf1b82508e704f1c8690a19a
cb-thushitamariaselvan <thushita.mariaselvan@chargebee.com>
2024-01-31 14:26:25 +0000

    Releasing v2.35.0. Please check the changelog for the updates.

--- lib/chargebee/models/invoice_estimate.rb
+++ lib/chargebee/models/invoice_estimate.rb
@@ -14,7 +14,7 @@ module ChargeBee
     end
 
     class LineItemTax < Model
-      attr_accessor :line_item_id, :tax_name, :tax_rate, :is_partial_tax_applied, :is_non_compliance_tax, :taxable_amount, :tax_amount, :tax_juris_type, :tax_juris_name, :tax_juris_code, :tax_amount_in_local_currency, :local_currency_code
+      attr_accessor :line_item_id, :tax_name, :tax_rate, :date_to, :date_from, :prorated_taxable_amount, :is_partial_tax_applied, :is_non_compliance_tax, :taxable_amount, :tax_amount, :tax_juris_type, :tax_juris_name, :tax_juris_code, :tax_amount_in_local_currency, :local_currency_code
     end
 
     class LineItemTier < Model
